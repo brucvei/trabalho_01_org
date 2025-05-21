ad5817d65040bc0dd0d3f5ebbed17bb1cad75048
Bruna Caetano <bruna.caetano@acad.ufsm.br>
2025-05-21 12:24:46 -0300

    loading files

--- main.s
+++ main.s
@@ -1,7 +1,12 @@
-        .data
+.data
 msg_init:   .asciiz "Inicializando o simulador MIPS...\n"
 msg_done:   .asciiz "Simulador inicializado com sucesso.\n"
 msg_pc_sp:  .asciiz "PC: 0x%08X, SP: 0x%08X\n"
+msg_load_text: .asciiz "Carregando instruções do arquivo binário...\n"
+msg_load_data: .asciiz "Carregando dados do arquivo binário...\n"
+filename_text: .asciiz "ex-000-073.bin"
+filename_data: .asciiz "ex-000-073.dat"
+err_file:   .asciiz "Erro ao abrir arquivo binário.\n"
 
         .text
         .globl main
@@ -33,6 +38,50 @@ init_regs:
         li $t3, 0x00400000
         sw $t3, PC
 
+        # Carrega arquivo binário de instruções em mem_text
+        la $a0, msg_load_text
+        li $v0, 4
+        syscall
+
+        la $a0, filename_text      # nome do arquivo
+        li $a1, 0                  # modo leitura
+        li $v0, 13                 # syscall open
+        syscall
+        bltz $v0, file_error
+        move $s0, $v0              # descritor do arquivo
+
+        la $a1, mem_text           # buffer destino
+        li $a2, 4096               # lê até 4096 bytes (ajuste conforme necessário)
+        li $v0, 14                 # syscall read
+        syscall
+        move $s1, $v0              # bytes lidos
+
+        li $v0, 16                 # syscall close
+        move $a0, $s0
+        syscall
+
+        # Carrega arquivo binário de dados em mem_data
+        la $a0, msg_load_data
+        li $v0, 4
+        syscall
+
+        la $a0, filename_data
+        li $a1, 0
+        li $v0, 13                 # syscall open
+        syscall
+        bltz $v0, file_error
+        move $s0, $v0
+
+        la $a1, mem_data
+        li $a2, 4096
+        li $v0, 14                 # syscall read
+        syscall
+        move $s2, $v0
+
+        li $v0, 16                 # syscall close
+        move $a0, $s0
+        syscall
+
         # Imprime mensagem de sucesso
         la $a0, msg_done
         li $v0, 4
@@ -42,7 +91,16 @@ init_regs:
         li $v0, 10
         syscall
 
+file_error:
+        la $a0, err_file
+        li $v0, 4
+        syscall
+        li $v0, 10
+        syscall
+
         .align 2
 reg:    .space 128  # 32 registradores de 4 bytes cada
 PC:     .word 0     # Contador de programa
 IR:     .word 0     # Registrador de instrução
+mem_text: .space 4096   # espaço para instruções (ajuste conforme necessário)
+mem_data: .space 4096   # espaço para dados (ajuste conforme necessário)
